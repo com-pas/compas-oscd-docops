c6e0d7681a3e09b1006692d1983aafc61b8202d1
Steffen van den Driest <35229971+Stef3st@users.noreply.github.com>
2023-03-23 09:31:36 +0100

    Move compas open (#5)
    * feat: moving components and dependencies
    it includes compas-open, compas-scl-list  and compas-scl-type-list (WIP)
    
    * test: wip tests for component needed
    
    Signed-off-by: Stef3st <steffen.van.den.driest@alliander.com>
    
    * chore: remove unused import
    
    * test: (wip) migrating tests from compas-scl-list into the menu plugin tests
    
    * test: (wip) finished migrating tests for compas-open
    
    Signed-off-by: Stef3st <steffen.van.den.driest@alliander.com>
    
    * fix: storybook (wip)
    chore: moving scl-list and scl-type-list to /helpers
    
    * chore: fixing rollup config and add missing deps
    
    * fix: workaround for showing dialog in storybook
    
    * fix: order of super calls
    
    * fix: catch type select event
    
    Signed-off-by: Stef3st <steffen.van.den.driest@alliander.com>
    
    * fix: reset plugin props on start
    
    * fix: labels icon in compas-scl-list
    
    * fix: screenshot test wrong import
    
    * fix: configure as esmodule instead of commonjs
    
    * fix: finishing up storybook stories
    
    * feat: added translations
    
    * test: update displayed message
    
    * fix: dialog 'closed' event listener
    
    * chore: remove unused comment & outdated openDoc fn
    
    * test: missing storybook case WithTypesList
    
    * fix: wrong path to compas-scl-type-list in a test
    
    * fix: taking screenshots out of gitignore
    
    * test: including baseline screenshots for diffing
    
    * ci: update screenshots
    
    * chore: adding failed screenshots to gitignore
    
    * chore: resolves comments opened by @pascalwilbrink
    
    * ci: update screenshots
    
    * test: incrementing thresholdPercentage
    
    * chore: closing subjects opened by @pascalwilbrink
    
    * ci: update screenshots
    
    * chore:triggering github actions again
    
    ---------
    
    Signed-off-by: Stef3st <steffen.van.den.driest@alliander.com>
    Co-authored-by: Juan Muñoz <juan.munoz@alliander.com>
    Co-authored-by: juancho0202 <juancho0202@users.noreply.github.com>

---
+++ src/to-be-made-a-dependency/foundation.ts
@@ -0,0 +1,63 @@
+import { get } from 'lit-translate';
+
+import { newOpenEvent } from '@openscd/open-scd-core';
+import {
+  COMPAS_SCL_PRIVATE_TYPE,
+  getCompasSclFileType,
+  getCompasSclName,
+  getPrivate,
+} from './compas-private.js';
+
+const FILE_EXTENSION_LENGTH = 3;
+
+export function getTypeFromDocName(docName: string): string {
+  if (
+    docName.lastIndexOf('.') ==
+    docName.length - (FILE_EXTENSION_LENGTH + 1)
+  ) {
+    return docName.substring(docName.lastIndexOf('.') + 1).toUpperCase();
+  }
+  throw new Error(get('compas.error.type'));
+}
+
+export function stripExtensionFromName(docName: string): string {
+  let name = docName;
+  // Check if the name includes a file extension, if the case remove it.
+  if (
+    name.length > FILE_EXTENSION_LENGTH &&
+    name.lastIndexOf('.') == name.length - (FILE_EXTENSION_LENGTH + 1)
+  ) {
+    name = name.substring(0, name.lastIndexOf('.'));
+  }
+  return name;
+}
+
+export function buildDocName(sclElement: Element): string {
+  const headerElement = sclElement.querySelector(':scope > Header');
+  const privateElement = getPrivate(sclElement, COMPAS_SCL_PRIVATE_TYPE);
+
+  const version = headerElement?.getAttribute('version') ?? '';
+  const name = getCompasSclName(privateElement)?.textContent ?? '';
+  const type = getCompasSclFileType(privateElement)?.textContent ?? 'SCD';
+
+  let docName = name;
+  if (docName === '') {
+    docName = headerElement?.getAttribute('id') ?? '';
+  }
+  docName += `-${version}.${type?.toLowerCase()}`;
+  return docName;
+}
+
+export function updateDocumentInOpenSCD(
+  element: Element,
+  doc: Document,
+  docName?: string
+): void {
+  const headerElement = doc.querySelector(':root > Header');
+  const id = headerElement?.getAttribute('id') ?? '';
+
+  // element.dispatchEvent(newLogEvent({ kind: 'reset' })); // TODO: figure out logging
+  element.dispatchEvent(
+    newOpenEvent(doc, docName ? docName : buildDocName(doc.documentElement))
+  );
+}
---
+++ test/menu/compas-open-plugin-scl-type-list.spec.ts
@@ -0,0 +1,137 @@
+import {
+  expect,
+  fixtureSync,
+  fixture,
+  html,
+  waitUntil,
+} from '@open-wc/testing';
+
+import sinon, { SinonStub } from 'sinon';
+import { ListItem } from '@material/mwc-list/mwc-list-item';
+import {
+  BASIC_TYPE_LIST_RESPONSE,
+  stubFetchResponseFunction,
+  TYPE_ENTRY_ELEMENT_NAME,
+} from './CompasSclDataServiceResponses.js';
+import '../../src/compas-ui/helpers/compas-scl-type-list.js';
+import CompasOpenMenuPlugin from '../../src/menu/compas-open-plugin.js';
+import '../../src/menu/compas-open-plugin.js';
+
+describe('compas-scltype-list', () => {
+  if (customElements.get('compas-open-plugin') === undefined)
+    customElements.define('compas-open-plugin', CompasOpenMenuPlugin);
+
+  let plugin: CompasOpenMenuPlugin;
+  let stub: SinonStub;
+  const FETCH_FUNCTION = 'fetchTypeList';
+
+  describe('show-loading', () => {
+    beforeEach(async () => {
+      plugin = await fixture(html`<compas-open-plugin></compas-open-plugin>`);
+
+      stub = stubFetchResponseFunction(
+        plugin,
+        FETCH_FUNCTION,
+        undefined,
+        TYPE_ENTRY_ELEMENT_NAME,
+        () => {
+          // Do nothing, so loading... will be displayed.
+        }
+      );
+
+      await plugin.updateComplete;
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('looks like the latest snapshot', async () => {
+      await expect(plugin).shadowDom.to.equalSnapshot();
+    });
+  });
+
+  describe('no-items-in-list', () => {
+    beforeEach(async () => {
+      plugin = fixtureSync<CompasOpenMenuPlugin>(
+        html`<compas-open-plugin></compas-open-plugin>`
+      );
+
+      stub = stubFetchResponseFunction(
+        plugin,
+        FETCH_FUNCTION,
+        undefined,
+        TYPE_ENTRY_ELEMENT_NAME,
+        (result: Element[]) => {
+          plugin.sclTypes = result;
+        }
+      );
+
+      await plugin.updateComplete;
+      await waitUntil(() => plugin.sclTypes !== undefined);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('looks like the latest snapshot', async () => {
+      await expect(
+        plugin.shadowRoot?.querySelector('compas-scltype-list')
+      ).shadowDom.to.equalSnapshot();
+      sinon.assert.calledOnce(stub);
+    });
+  });
+
+  describe('after-list-loaded', () => {
+    beforeEach(async () => {
+      plugin = fixtureSync<CompasOpenMenuPlugin>(
+        html`<compas-open-plugin></compas-open-plugin>`
+      );
+
+      stub = stubFetchResponseFunction(
+        plugin,
+        FETCH_FUNCTION,
+        BASIC_TYPE_LIST_RESPONSE,
+        TYPE_ENTRY_ELEMENT_NAME,
+        (result: Element[]) => {
+          plugin.sclTypes = result;
+        }
+      );
+
+      await plugin.updateComplete;
+      await waitUntil(() => plugin.sclTypes !== undefined);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('has 2 item entries', () => {
+      expect(
+        plugin.shadowRoot
+          ?.querySelector('compas-scltype-list')
+          ?.shadowRoot?.querySelectorAll('mwc-list > mwc-list-item')
+      ).to.have.length(2);
+    });
+
+    it('selecting the first row will cause list scl method to be called', async () => {
+      const eventSpy = sinon.spy();
+      plugin.addEventListener('typeSelected', eventSpy);
+
+      (<ListItem>(
+        plugin.shadowRoot
+          ?.querySelector('compas-scltype-list')
+          ?.shadowRoot?.querySelectorAll('mwc-list > mwc-list-item')[0]
+      )).click();
+      await plugin.updateComplete;
+
+      sinon.assert.calledOnce(eventSpy);
+    });
+
+    it('looks like the latest snapshot', async () => {
+      await expect(plugin).shadowDom.to.equalSnapshot();
+      sinon.assert.calledOnce(stub);
+    });
+  });
+});
